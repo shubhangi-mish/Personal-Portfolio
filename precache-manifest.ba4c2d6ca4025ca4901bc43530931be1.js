40b0844ec9b1251f151ecdf8bdd3da83ff35e9a8
Shubhangi Mishra <shubhangimishra3302@gmail.com>
2024-06-03 19:09:31 +0530

    Updates

---
+++ precache-manifest.ba4c2d6ca4025ca4901bc43530931be1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cb2142b57cb6ac08c31042e0212b9d",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "4edf7c707dbf47a740db",
+    "url": "/Personal-Portfolio/static/css/2.6f339755.chunk.css"
+  },
+  {
+    "revision": "324e880464d5a35bc9e3",
+    "url": "/Personal-Portfolio/static/css/main.726a7cbc.chunk.css"
+  },
+  {
+    "revision": "4edf7c707dbf47a740db",
+    "url": "/Personal-Portfolio/static/js/2.68a73d1f.chunk.js"
+  },
+  {
+    "revision": "c8c38381e12ec9a63b77",
+    "url": "/Personal-Portfolio/static/js/3.89dfd605.chunk.js"
+  },
+  {
+    "revision": "324e880464d5a35bc9e3",
+    "url": "/Personal-Portfolio/static/js/main.5804d404.chunk.js"
+  },
+  {
+    "revision": "32f94313b72d93eff935",
+    "url": "/Personal-Portfolio/static/js/runtime~main.fa3c4549.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/Personal-Portfolio/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/Personal-Portfolio/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "a8e2a364a39bed418460cbfe607f2c7c",
+    "url": "/Personal-Portfolio/static/media/banner-bg.a8e2a364.png"
+  },
+  {
+    "revision": "7c8d809c758959e1a4ce1bc44419c85d",
+    "url": "/Personal-Portfolio/static/media/color-sharp.7c8d809c.png"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/Personal-Portfolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "03a06a92c70e4582dcc7e29529b829aa",
+    "url": "/Personal-Portfolio/static/media/header-img2.03a06a92.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Personal-Portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/Personal-Portfolio/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/Personal-Portfolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/Personal-Portfolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/Personal-Portfolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/Personal-Portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/Personal-Portfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/Personal-Portfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Personal-Portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Personal-Portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Personal-Portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "02233e5a7cb7757985d3dfd0da91587a",
+    "url": "/Personal-Portfolio/static/media/skills-bg.02233e5a.png"
+  }
+]);
